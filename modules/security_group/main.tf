5fbcaa8a3096dd40f8976c463f1664c501508721
Wenbo Li <liwenbo55@live.com>
2023-12-25 21:29:38 +1100

    terrafom fmt.

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
