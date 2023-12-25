fa3c1d4a33e2832ecbb2a22780bae6f3da899948
Wenbo Li <liwenbo55@live.com>
2023-12-25 23:07:48 +1100

    Update ALB module.

--- modules/ALB/main.tf
+++ modules/ALB/main.tf
@@ -71,11 +71,6 @@ resource "aws_lb_listener" "backendalb_443" {
 ##########################################################################
 resource "aws_route53_record" "a_record_for_alb" {
   zone_id = var.hosted_zone_id
-  # This for project
-  # name    = "www.backend.${var.environment}.${var.project_name}.com" 
-  # This for my test
-  # name    = "backend.${var.environment}.wenboli.xyz"
-  # new branch test
   name = "backend.${var.environment}.${var.hosted_zone_name}"
   type = var.record_type_A
 
@@ -90,8 +85,6 @@ resource "aws_route53_record" "a_record_for_alb" {
 # Add SSL certificate for load balancer A record
 ##########################################################################
 resource "aws_acm_certificate" "acm_certificate" {
-  # domain_name               = "www.backend.${var.environment}.${var.project_name}.com"
-  # domain_name               = "backend.${var.environment}.wenboli.xyz"
   domain_name = "backend.${var.environment}.${var.hosted_zone_name}"
   # subject_alternative_names = ["backend.${var.environment}.${var.project_name}.com"]
   validation_method = var.validation_method
