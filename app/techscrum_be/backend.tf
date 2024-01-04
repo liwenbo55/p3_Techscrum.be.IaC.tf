f67a462489952b98c6099c8c047f79fbbc22f908
Wenbo Li <liwenbo55@live.com>
2024-01-04 16:37:25 +1100

    Update backend file.

--- app/techscrum_be/backend.tf
+++ app/techscrum_be/backend.tf
@@ -1,15 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "p3-crankbit-backend-terraform.statefile"
-#     key            = "backend-terraform.l.tfstate"
-#     region         = "ap-southeast-2"
-#     dynamodb_table = "crankbit-backend-terraform.state-lock-l"
-#     profile        = "credentials_p3_crankbit"
-#   }
-# }
-
-###################################################################################################
-
 terraform {
 
   backend "s3" {
