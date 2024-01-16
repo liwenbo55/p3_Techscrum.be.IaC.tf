53d8a1168300c7cf2451d6cdc9b5d03e53fcdbc3
Wenbo Li <liwenbo55@live.com>
2024-01-16 22:11:24 +1100

    Terraform format.

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -76,6 +76,7 @@ resource "aws_route_table_association" "public_subnet_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_routetable.id
 }
+
 resource "aws_route_table_association" "private_subnet_1" {
   count          = var.environment == "prod" || var.environment == "uat" ? 1 : 0
   subnet_id      = aws_subnet.private_subnet_1[count.index].id
